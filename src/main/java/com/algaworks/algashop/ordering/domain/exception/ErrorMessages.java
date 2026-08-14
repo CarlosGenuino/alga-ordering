27bac0cd10b877b5c1dde5151cd3a01e0af47f8a
Carlos Genuino <carloxsgenuino3@live.com>
2026-08-13 21:47:31 -0300

    Order e OrderStatus

--- src/main/java/com/algaworks/algashop/ordering/domain/exception/ErrorMessages.java
+++ src/main/java/com/algaworks/algashop/ordering/domain/exception/ErrorMessages.java
@@ -11,4 +11,6 @@ public class ErrorMessages {
     public static final String VALIDATION_ERROR_PHONE_IS_NULL = "Phone cannot be null";
     public static final String VALIDATION_ERROR_PHONE_IS_BLANK = "Phone cannot be blank";
     public static final String VALIDATION_ERROR_ZIP_CODE_INVALID = "Zip code is invalid";
+    public static final String ERROR_ORDER_STATUS_CANNOT_BE_CHANGED = "Cannot change order %s status from %s to %s";
+
 }
